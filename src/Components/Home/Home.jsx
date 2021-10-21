7bb68f104fe3f7c3eed0500d4cd78e1270c4248f
ricardo <ricardohsweiss@gmail.com>
2021-10-21 11:29:28 -0300

    not allow user to fetch empty input, style search button

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -25,6 +25,9 @@ function Home() {
     const emptyList = pokemonAbilities.length <= 0
 
     function fetchPokemon() {
+        if (inputValue === '') {
+            return;
+        }
         setError(false)
         setPokemonAbilities([])
         setLoading(true);
@@ -54,7 +57,7 @@ function Home() {
     return (
         <Container>
             <HomeTitle>Please type a pokemon name below to see their abilities</HomeTitle>
-            <Input type="text" value={inputValue} onChange={onChangeHandler}/>
+            <Input placeholder={'Type here'} type="text" value={inputValue} onChange={onChangeHandler}/>
             <Button onClick={fetchPokemon}>
                 <ButtonText>Search</ButtonText>
             </Button>
--- src/Components/Home/Home.styles.jsx
+++ src/Components/Home/Home.styles.jsx
@@ -15,25 +15,42 @@ export const HomeTitle = styled.h2`
 export const Input = styled.input`
   width: 80%;
   height: 30px;
-  border: 1px solid black;
-  border-radius: 10px;
+  border: none;
+  border-bottom: 1px solid black;
   margin-bottom: 32px;
+  max-width: 500px;
+  &:focus {
+    outline: none;
+  }
 `;
 
 export const Button = styled.button`
-  width: 120px;
+  position: relative;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 40px;
-  border-radius: 10px;
-  border: 1px solid grey;
+  width: 160px;
+  height: 30px;
   margin-bottom: 32px;
-  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+  padding: 1.5rem 3.125rem;
+  background-color: #3498db;
+  border: none;
+  border-radius: 0.3125rem;
+  box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.2);
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    background-color: #324c5d
+  }
 `;
 
 export const ButtonText = styled.p`
   font-size: 16px;
+  color: white
 `;
 
 export const ErrorContainer = styled.div`
